3128ed096f3c7bc50a090b2e08ec0b64226b6999
smallstrong0 <393019766@qq.com>
2018-10-18 22:39:33 +0800

    【ss】config module: add config comments.

--- ss/config.py
+++ ss/config.py
@@ -8,6 +8,10 @@
 
 import random
 
-DELAY = 0.5 + random.random()
-NUM_RETRIES = 5
-NS = 'ss'
+DELAY = 0.5 + random.random()  # 爬虫延时
+NUM_RETRIES = 5  # 链接重试次数
+NS = 'ss'  # redis缓存前缀
+PROCESS_LIMIT_NUM = 10  # 开多少进程数量
+COROUTINE_LIMIT_NUM = 10  # 单进程开启协程的数量
+FLAG = True  # 进程是否开启的标志
+LOG_FILE_NAME = 'test'  # 日志文件名
