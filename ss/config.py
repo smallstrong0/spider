1040387958aec10d501b5c481411ef284035c799
smallstrong0 <393019766@qq.com>
2018-10-19 11:36:24 +0800

    【ss】config module: add describe.
    good article: https://segmentfault.com/a/1190000005840672

--- ss/config.py
+++ ss/config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2018/10/16 18:16
 # @Author  : SmallStrong
-# @Des     : 
+# @Des     : 爬虫要适当温柔，不能太生猛，不好不好。
 # @File    : config.py
 # @Software: PyCharm
 
@@ -11,7 +11,7 @@ import random
 DELAY = 0.5 + random.random()  # 爬虫延时
 NUM_RETRIES = 5  # 链接重试次数
 NS = 'ss'  # redis缓存前缀
-PROCESS_LIMIT_NUM = 10  # 开多少进程数量
-COROUTINE_LIMIT_NUM = 10  # 单进程开启协程的数量
+PROCESS_LIMIT_NUM = 1  # 开多少进程数量
+COROUTINE_LIMIT_NUM = 1  # 单进程开启协程的数量
 FLAG = True  # 进程是否开启的标志
-LOG_FILE_NAME = 'test'  # 日志文件名
+LOG_FILE_NAME = 'default'  # 日志文件名
