4ff9f3c1692fd97bb7150f023afbeb3664d12698
smallstrong0 <393019766@qq.com>
2018-11-08 20:57:17 +0800

    【baihu】zhihu init

---
+++ baihu/zhihu/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/8 20:56
+# @Author  : SmallStrong
+# @Des     : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ baihu/zhihu/zhihu.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/8 20:56
+# @Author  : SmallStrong
+# @Des     : 
+# @File    : zhihu.py
+# @Software: PyCharm
